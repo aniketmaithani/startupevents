8e8747ad8a0440aba2cf7d0bc65db395972ea5fb
Aniket Maithani <maithani.aniket@gmail.com>
2017-07-29 13:07:43 +0530

    fix(urls.py): fixed non-commented line

--- startupevents/urls.py
+++ startupevents/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from .profiles.views import Registration
 
-Sourabh is here
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
